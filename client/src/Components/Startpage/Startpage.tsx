bcf381aac5ec8bcef8a62a9a374e69347221a2ce
OGALEXI <64463918+OGALEXI@users.noreply.github.com>
2021-11-26 16:17:18 +0100

    chore: add logo asset and startpage component

---
+++ client/src/Components/Startpage/Startpage.tsx
@@ -0,0 +1,11 @@
+import { ReactComponent as QRLogo } from '../Assets/PerfectLogo.svg';
+
+const Startpage = () => {
+    return (
+        <div>
+            <div><QRLogo /></div>
+        </div>
+    )
+}
+
+export default Startpage;
