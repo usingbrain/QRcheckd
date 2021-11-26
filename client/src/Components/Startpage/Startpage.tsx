567001c235c6f53c836b48f5c1654f50350fc66c
OGALEXI <64463918+OGALEXI@users.noreply.github.com>
2021-11-26 20:49:39 +0100

    chore: add tailwind config

--- client/src/Components/Startpage/Startpage.tsx
+++ client/src/Components/Startpage/Startpage.tsx
@@ -1,8 +1,7 @@
 import { ReactComponent as QRLogo } from '../Assets/PerfectLogo.svg';
-import './Startpage.css';
 
 const buttonStyle = "text-white h-12 w-32 md:h-20 md:w-56 lg:w-72 border-2 border-white my-2";
-const buttonWrapper = "register-login flex rounded-sm max-w-sm h-30 w-60 md:w-80 md:h-56 lg:w-96 lg:h-60 xl:w-1/2 xl:h-80 float-right mx-16 justify-center items-center flex-col p-4";
+const buttonWrapper = "bg-black flex rounded-sm max-w-sm h-60 w-60 md:w-80 md:h-56 lg:w-96 lg:h-60 xl:w-96 xl:h-80 sm:float-right sm:mx-16 justify-center items-center flex-col p-4 mx-auto";
 
 const Startpage = () => {
     return (
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,45 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      green: {
+        DEFAULT: '#00A393'
+      },
+      turqoise: {
+        DEFAULT: '#69FFF1'
+      },
+      white: {
+        DEFAULT: '#FFFBFC'
+      },
+      grey: {
+        DEFAULT: '#535353'
+      },
+      black: {
+        DEFAULT: '#010400'
+      }
+    },
+    extend: {
+      backgroundOpacity: {
+        '30': '0.3'
+      },
+      width: {
+        '100': '400px',
+        '110': '430px',
+        '120': '450px',
+        '130': '500px'
+      }
+    },
+    screens: {
+      'tablet': '272px',
+      'xs': '440px',
+      'sm': '650px',
+      'md': '790px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1350px',
+      '3xl': '1530px',
+      '4xl': '1800px'
+    }
   },
   variants: {
     extend: {},
