179b4334ee7e3c753d571f0d153459c6d85e80d4
BartoszSzatkowski <szatek24@gmail.com>
2021-11-30 15:08:08 +0100

    chore: add QrScanner working package

--- client/src/Components/StudentView/StudentDashboard.tsx
+++ client/src/Components/StudentView/StudentDashboard.tsx
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
+import QrReader from 'react-qr-reader';
 
 const bakcgroudnStyle = 'bg-green h-screen';
 
-interface ScannerState {
-  value: string;
-  watching: boolean;
-}
-
 const StudentDashboard: React.FC = () => {
-  const [scanner, setScanner] = useState<ScannerState>({
-    value: '',
-    watching: false,
-  });
+  const [displayScanner, setDisplayScanner] = useState(false);
+  const handleData = (data: any) => {
+    console.log(data);
+  };
+
+  const handleError = (error: any) => {
+    console.log(error);
+  };
 
   return (
     <div className={bakcgroudnStyle}>
       <div>Welcome undefined</div>
       <div>Scan QR</div>
+      {displayScanner && (
+        <QrReader
+          delay={300}
+          onError={handleError}
+          onScan={handleData}
+          style={{ width: '30%' }}
+        />
+      )}
+      <button onClick={() => setDisplayScanner(!displayScanner)}>Scan?</button>
     </div>
   );
 };
