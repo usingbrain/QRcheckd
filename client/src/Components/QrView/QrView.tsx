ee44e9416660c8c5edc2ff1ea5c44e7337dcf46c
Maria Sudermann <maria.sudermann@gmail.com>
2021-12-02 20:10:55 +0100

    style: style qr view

--- client/src/Components/QrView/QrView.tsx
+++ client/src/Components/QrView/QrView.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import QRCode from 'react-qr-code';
 
-const QrView: React.FC<{ session: number }> = ({ session }) => {
+const QrView: React.FC<{ sessionId: number }> = ({ sessionId }) => {
   // generate QR code
   // const qrValue = session;
   const qrValue = 'session';
 
   return (
     // display QR code
-    <main>
+    <main className="w-screen h-screen flex flex-row justify-center items-center">
       <section>
         <QRCode value={qrValue} />
       </section>
+      {/* maybe display a timer for how long the qr code has been open? */}
     </main>
   );
 };
