11993b788071a4716a195f49f109a3d9b109d17b
OGALEXI <64463918+OGALEXI@users.noreply.github.com>
2021-12-04 14:38:53 +0100

    chore: new teacher homepage styling

--- client/src/Components/TeacherDashboard/Sidemenu.tsx
+++ client/src/Components/TeacherDashboard/Sidemenu.tsx
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { setForm } from '../../store/actions';
 import CourseList from './CourseList';
+import { ReactComponent as QRLogo } from '../../Assets/thePerfectestLogo2.svg';
 
 const sidemenuStyle =
-  'bg-white flex flex-col justify-start items-center w-1/4 pt-4 h-screen';
-const btnStyle = 'bg-grey hover:bg-black p-3 rounded mb-4 w-3/5 text-center';
-const btnTextStyle = 'text-white font-bold text-sm';
+  'bg-green flex flex-col justify-start items-center w-full h-screen';
+const btnStyle = 'bg-white hover:bg-green-light p-3 rounded-sm my-4 w-3/5 text-center';
+const btnTextStyle = 'text-green font-bold text-sm';
+const logoStyle = " flex w-full h-20 bg-white justify-center shadow-b-lg"
 
 const Sidemenu: React.FC = () => {
   const open = useSelector((state: { form: boolean }) => state.form);
@@ -15,13 +17,17 @@ const Sidemenu: React.FC = () => {
 
   return (
     <nav className={sidemenuStyle}>
+      <Link to='/' className={logoStyle}>
+        <QRLogo className="w-2/3 lg:w-1/2 flex" />
+      </Link>
       <div className={btnStyle}>
         <Link to="/homepage/new-course">
           <button
             onClick={() => dispatch(setForm(open))}
             className={btnTextStyle}
           >
-            ADD NEW CLASS
+            <p className="invisible sm:visible h-0 sm:h-full w-0 sm:w-full">ADD NEW CLASS</p>
+            <p className="text-5xl visible sm:invisible h-full sm:h-0">+</p>
           </button>
         </Link>
       </div>
--- client/src/Components/TeacherHomepage/Navbar.tsx
+++ client/src/Components/TeacherHomepage/Navbar.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
-import logo from '../../Assets/thePerfectestLogo2.svg';
-=======
-import logo from '../../Assets/PerfectLogo2.svg';
 import User from '../../Types/user';
 import { useNavigate } from 'react-router';
 import { useLogoutMutation } from '../../generated/graphql';
->>>>>>> a0c2d23f11a9aed49a2d916fd6d107ea467d089f
 
-const navStyle = 'flex flex-row justify-between h-20 p-4';
+const navStyle = 'flex flex-row justify-end h-20 p-4 w-3/4 float-right shadow-lg';
 
 const Navbar: React.FC<{ user: User }> = ({ user }) => {
   const [, logout] = useLogoutMutation();
@@ -23,15 +18,7 @@ const Navbar: React.FC<{ user: User }> = ({ user }) => {
   };
   return (
     <nav className={navStyle}>
-<<<<<<< HEAD
-      <Link to="/">
-        <img src={logo} alt="qrcheckd logo" className="w-4/6" />
-=======
-      <Link to='/'>
-        <img src={logo} alt='qrcheckd logo' className='h-full' />
->>>>>>> a0c2d23f11a9aed49a2d916fd6d107ea467d089f
-      </Link>
-      <section className='flex flex-row justify-end items-center'>
+      <section className='flex flex-row items-center'>
         <h3 className='pr-8 font-lato font-bold text-lg'>
           {user.name} {user.lastname}
         </h3>
