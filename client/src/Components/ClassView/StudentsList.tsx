44378c81b220e9cb325d27a8f34cb7cf2b312d33
BartoszSzatkowski <szatek24@gmail.com>
2021-12-06 10:12:48 +0100

    feat: refetch class assigment on new assigment

--- client/src/Components/ClassView/StudentsList.tsx
+++ client/src/Components/ClassView/StudentsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useAssignedStudentsQuery } from '../../generated/graphql';
 import Lottie from 'react-lottie';
@@ -6,18 +6,27 @@ import loadingAnimation from '../../Assets/loadinganimation.json';
 import StudentElement from './StudentElement';
 import CheckboxList from './CheckboxList';
 import Session from '../../Types/session';
+import socketIOClient from 'socket.io-client';
+const ENDPOINT = 'http://localhost:4000';
 
 const listStyle = 'flex flex-col justify-start items-start';
 
 const StudentsList: React.FC<{ courseId: number }> = ({ courseId }) => {
-  const [{ fetching, data, error }] = useAssignedStudentsQuery({
-    variables: { courseId },
-  });
+  const [{ fetching, data, error }, refetchAssigned] = useAssignedStudentsQuery(
+    {
+      variables: { courseId },
+      requestPolicy: 'network-only',
+    }
+  );
   const students = data?.getAssignedStudents.data;
-
   const session = useSelector(
     (state: { session: Session | null }) => state.session
   );
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on('ATTENDANCE_CHANGE', () => refetchAssigned());
+  }, []);
 
   const loadingAnimationOptions = {
     loop: true,
@@ -36,7 +45,7 @@ const StudentsList: React.FC<{ courseId: number }> = ({ courseId }) => {
   return (
     <div className={listStyle}>
       {students?.length ? (
-        <div className="flex">
+        <div className='flex'>
           <div>
             {students.map((student) => {
               return (
