786df636756e8f366ef5d7d53513b85ca511f2db
OGALEXI <64463918+OGALEXI@users.noreply.github.com>
2021-12-07 13:07:34 +0100

    chore: styling calendar

--- client/src/Components/TeacherHomepage/Homepage.tsx
+++ client/src/Components/TeacherHomepage/Homepage.tsx
@@ -10,7 +10,7 @@ const Homepage: React.FC = () => {
   return (
     <div>
       <Navbar user={user} />
-      <main className="flex flex-row justify-start w-full">
+      <main className="flex flex-row justify-start w-full h-screen">
         <Sidemenu />
         <Outlet />
       </main>
